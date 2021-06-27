66f0c6ef2f0d26bd5960e4362f03e3c713de1238
Aleksandrov Sergey <Aleks140988@gmail.com>
2021-06-28 02:11:17 +0300

    first commit

---
+++ src/components/Buttons/Btn.vue
@@ -0,0 +1,72 @@
+<template>
+
+    <button
+      :class="`btn ${additionalClass} ${textColor}`"
+    >{{ textBtn }}</button>
+
+</template>
+
+<script>
+export default {
+  name: 'Btn',
+  props: {
+    additionalClass: String,
+    textBtn: String,
+    textColor: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.btn {
+  transition: background-color 0.3s;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  min-width: 200px;
+  min-height: 40px;
+  border-radius: 40px;
+  padding: 10px 20px;
+  font-size: 14px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  font-weight: 600;
+  background-color: transparent;
+  cursor: pointer;
+
+  &--bg-primary {
+    background-color: blue;
+
+    &:hover {
+      background-color: orange;
+    }
+  }
+
+  &--bg-secondary {
+    background-color: orange;
+
+    &:hover {
+      background-color: blue;
+    }
+  }
+
+  &--color-text-primary {
+    color: #fff;
+  }
+
+  &--color-text-secondary {
+    color: blue;
+  }
+
+  &--outline {
+    border: 1px solid blue;
+
+    &:hover {
+      background-color: blue;
+      color: #fff;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,34 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <btn
+      additionalClass="btn--bg-primary"
+      textBtn="primary text"
+      textColor="btn--color-text-primary"
+    ></btn>
+    <btn
+      additionalClass="btn--bg-secondary"
+      textBtn="secondary text"
+      textColor="btn--color-text-primary"
+    ></btn>
+    <btn
+      additionalClass="btn--outline"
+      textBtn="secondary text"
+      textColor="btn--color-text-secondary"
+    ></btn>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld'
+import Btn from '@/components/Buttons/Btn'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Btn
   }
 }
 </script>
